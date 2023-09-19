63a19e41accdfc4d31d679eb3a0b36e7cf1edc24
Irem <91956073+iremkose38@users.noreply.github.com>
2023-09-19 20:49:17 +0200

    deletions and testing

--- scraper.py
+++ scraper.py
@@ -2,20 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 
 def scrape_movies(year, genre):
-    # Define the URL for IMDb's search page with user-selected parameters
-    url = f"http://www.imdb.com/search/title?release_date={year}&title_type=feature&genres={genre}"
+    # Check if year is not a number or genre is empty
+    if not year.isdigit() or not genre:
+        return []
 
-    # Send an HTTP GET request to the URL
+    url = f"http://www.imdb.com/search/title?release_date={year}-01-01,{year}-12-31&title_type=feature&genres={genre}"
     response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    # Extract movie data, including title, rating, and other details
     movie_data = []
-    for movie in soup.find_all("div", class_="lister-item mode-advanced"):
+
+    for movie in soup.find_all("div", class_="lister-item-content"):
         title = movie.h3.a.text
-        rating = movie.strong.text
-        movie_data.append({"title": title, "rating": rating})
+        rating = movie.strong
+        if rating:
+            rating = rating.text
+        else:
+            rating = "N/A"
+
+        movie_data.append({
+            "title": title,
+            "rating": rating,
+        })
 
     return movie_data
---
+++ testing.py
@@ -0,0 +1,30 @@
+import unittest
+import scraper
+
+class TestScraper(unittest.TestCase):
+
+    def test_scrape_movies(self):
+        # Test scraping movies for a specific year and genre
+        year = "2020"
+        genre = "Action"
+        movie_data = scraper.scrape_movies(year, genre)
+
+        # Assert that the scraped data is not empty
+        self.assertTrue(len(movie_data) > 0)
+
+        # Check if the data has the expected keys (title, rating)
+        for movie in movie_data:
+            self.assertIn("title", movie)
+            self.assertIn("rating", movie)
+
+    def test_scrape_movies_invalid_input(self):
+        # Test scraping movies with invalid input (non-numeric year and empty genre)
+        invalid_year = "invalid_year"
+        invalid_genre = ""
+        movie_data = scraper.scrape_movies(invalid_year, invalid_genre)
+
+        # Assert that the scraped data is empty for invalid input
+        self.assertEqual(len(movie_data), 0)
+
+if __name__ == "__main__":
+    unittest.main()
